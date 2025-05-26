0c30fc49ba078ebaf4b557c0e9eda4f1d27906b1
Alex Jumla <alex@jumlajumla.com>
2025-05-27 01:32:52 +0300

    Add reCAPTCHA debugging and cache-busting

--- src/components/recaptcha.js
+++ src/components/recaptcha.js
@@ -3,13 +3,21 @@ import ReCAPTCHA from "react-google-recaptcha";
 import "./recaptcha.css";
 
 const Recaptcha = ({ onChange }) => {
+  // Debug: Log the reCAPTCHA key being used
+  console.log('Using reCAPTCHA site key:', RECAPTCHASITEKEY);
+  
   const handleRecaptchaChange = (value) => {
     // Pass the reCAPTCHA response value to the parent component
+    console.log('reCAPTCHA response received');
     onChange(value);
   };
 
+  // Add cache-busting query parameter
+  const recaptchaUrl = `https://www.google.com/recaptcha/api.js?render=explicit&_=${new Date().getTime()}`;
+  
   return (
     <div className="recaptcha-wrapper">
+      <script src={recaptchaUrl} async defer></script>
       <ReCAPTCHA
         sitekey={RECAPTCHASITEKEY}
         onChange={handleRecaptchaChange}
